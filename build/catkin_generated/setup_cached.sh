e847badab41c2af3b5f5996e99029ffebef38123
DevWalrus <70732411+DevWalrus@users.noreply.github.com>
2022-09-15 17:53:08 -0400

    Added OpenCV Package

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/walrusdev/Documents/Ultima/Ultima/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/walrusdev/Documents/Ultima/Ultima/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/walrusdev/Documents/Ultima/Ultima/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/walrusdev/Documents/Ultima/Ultima/src:$ROS_PACKAGE_PATH"
---
+++ build/navx/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/walrusdev/Documents/Ultima/Ultima/src/navx
+# Build directory: /home/walrusdev/Documents/Ultima/Ultima/build/navx
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
