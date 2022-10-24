aebdcd6d2f39b21c70eaca47eaf87603e2fdd984
DevWalrus <70732411+DevWalrus@users.noreply.github.com>
2022-10-24 13:02:58 -0400

    test2

--- install/camera_lite_interfaces/share/camera_lite_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(camera_lite_interfaces_IDL_FILES "srv/Save.idl")
-set(camera_lite_interfaces_INTERFACE_FILES "srv/Save.srv;srv/Save_Request.msg;srv/Save_Response.msg")
