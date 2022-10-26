71ed8e0eac8a6cdd4f63bbd4553d92c24f90a595
Jake Pregitzer <jwp2545@rit.edu>
2022-10-25 20:07:31 -0400

    Fix bug

--- src/image_read/image_read/image_read.py
+++ src/image_read/image_read/image_read.py
@@ -11,9 +11,10 @@ import cv2
 import os
 import sys
 
+
 class MinimalSubscriber(Node):
 
-    def __init__(self):
+    def __init__(self, directory):
         super().__init__('minimal_subscriber')
         self.subscription = self.create_subscription(
             Image,
@@ -24,14 +25,16 @@ class MinimalSubscriber(Node):
         self.br = CvBridge()
         self.count = 0
         self.image_number = 1
+        self.directory = directory
 
     def listener_callback(self, data):
         self.get_logger().info('Reading an image')
-        
+
         current_frame = self.br.imgmsg_to_cv2(data)
 
         # save cv2 image
         if self.count % 10000 == 0:
+            os.chdir(self.directory)
             cv2.imwrite('camera_image' + str(self.image_number) + '.jpeg', current_frame)
             self.image_number += 1
         else:
@@ -39,16 +42,15 @@ class MinimalSubscriber(Node):
 
         # Display image
         cv2.imshow("camera", current_frame)
-    
+
         cv2.waitKey(1)
 
 
 def main(args=None):
     rclpy.init(args=args)
     directory = sys.argv[1]
-    os.chdir(directory)
 
-    minimal_subscriber = MinimalSubscriber()
+    minimal_subscriber = MinimalSubscriber(directory)
 
     rclpy.spin(minimal_subscriber)
 
