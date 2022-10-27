e0339742ef37887d2e8c77f97c987727921101b0
ardit <ak1935@g.rit.edu>
2022-10-27 19:08:05 -0400

    Added conversion and show, run imageview class.

--- src/image_read/image_read/image_view.py
+++ src/image_read/image_read/image_view.py
@@ -1,12 +1,10 @@
-# ARDIT
-
 import torch
 import torchvision
 from torchvision.io import read_image
 import torchvision.transforms as transforms
 import rclpy
 from rclpy.node import Node
-
+from PIL import Image
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
@@ -15,5 +13,45 @@ import sys
 from datetime import datetime
 
 
-class PyTorchSubscriber(Node):
-    pass
+class MinimalSubscriber(Node):
+
+    def __init__(self, directory, frames):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            Image,
+            '/camera/image_raw',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+        self.br = CvBridge()
+        self.count = 0
+        self.image_number = 1
+        self.directory = os.path.join(directory, str(datetime.now()))
+        os.mkdir(self.directory)
+        self.frames = frames
+
+    def listener_callback(self, data):
+        current_frame = self.br.imgmsg_to_cv2(data)
+        transform = transforms.ToTensor()
+        tensor_image = transform(current_frame)
+        print(tensor_image)
+        cv2.waitKey(1)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    if not os.path.exists(sys.argv[1]):
+        exit()
+    minimal_subscriber = MinimalSubscriber(sys.argv[1], int(sys.argv[2]))
+
+    rclpy.spin(minimal_subscriber)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
